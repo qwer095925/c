418e54b5e12748638163843c70d2bc917216e179
qwer095925 <92494763+qwer095925@users.noreply.github.com>
2021-10-15 20:50:27 +0800

    Create 两个数的简单计算器.c

---
+++ 两个数的简单计算器.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    char c;
+    scanf("%d %c %d",&a,&c,&b);
+    switch(c)
+    {
+        case'+':printf("%d\n",a+b);break;
+        case'-':printf("%d\n",a-b);break;
+        case'*':printf("%d\n",a*b);break;
+        case'/':printf("%d\n",a/b);break;
+        case'%':printf("%d\n",a%b);break;
+        default:printf("ERROR");
+        return 0;
+    }
+    
+}
